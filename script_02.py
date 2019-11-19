9b8ea416690251eacbadcc05d5bab441b4e64a5e
Artem <a.selekhov@gmail.com>
2019-11-19 10:27:07 +0300

    Rebuild script_02.py and the results of 3 gif files

--- script_02.py
+++ script_02.py
@@ -1,5 +1,21 @@
 #script_02.py
 import requests
+gif_list = ["fail", "people", "lucky"]
+BASE_URL = "https://api.giphy.com/v1/stickers/random"
+for gif in gif_list:
+    params = dict(
+        api_key="dc6zaTOxFJmzC",
+        tag="lucky",
+        rating="g",
+        fmt='json'
+        )
+    response = requests.get(BASE_URL, params=params)
+    print(response.ok)
+    gif_url = response.json()['data']['fixed_height_small_url']
+    resp_gif = requests.get(gif_url)
+    with open(gif+".gif", "wb") as f:
+        f.write(resp_gif.content)#script_02.py
+import requests
 
 BASE_URL = "https://api.giphy.com/v1/stickers/random"
 params = dict(
